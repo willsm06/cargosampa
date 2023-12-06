ae381d7f71cb8bf6f8136ac5f6862ea9c4cda494
willsm06 <williamsm.06@hotmail.com>
2023-12-06 00:28:43 -0300

    add sobre

---
+++ sobre.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/sobre.css">
+    <title>Home - CargoSampa</title>
+</head>
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <main>
+        <div class="presentation_wrapper">
+            <img src="assets/img/gif_sobre.gif" alt="embarcacao_image">
+            <div class="overlay">
+            </div>
+        </div>
+        <div class="content_home">
+            <div class="white-box">
+                <p>Somos os melhores!</p>
+            </div>
+            <div class="description">
+            </div>
+            <h2>Um pouco sobre nosso serviço</h2>
+            <div class="content_boxes_one">
+                <div class="content_boxes_one_text">
+                    A concepção inovadora da nossa embarcação se baseia em uma rota única, proporcionando uma eficiência
+                    logística sem precedentes. Nesse modelo singular, a embarcação realiza paradas estrategicamente
+                    planejadas, permitindo tanto o carregamento quanto o descarregamento de cargas nos pontos designados
+                    ao longo do trajeto. Essa abordagem pioneira possibilita a reserva prévia de espaços na embarcação,
+                    otimizando a capacidade de transporte e proporcionando um planejamento logístico mais preciso e
+                    flexível.
+                    <br>
+                    <br>
+                    Ao permitir que empresas reservem antecipadamente seus espaços na embarcação, criamos um sistema que
+                    maximiza a utilização dos recursos disponíveis. Essa estratégia não só agiliza o processo de carga e
+                    descarga, mas também oferece aos clientes a garantia de espaço dedicado para suas mercadorias. Isso
+                    resulta em maior eficiência operacional, redução de tempos de espera e, consequentemente, uma
+                    logística mais ágil e econômica.
+                    <br>
+                    <br>
+                    Além disso, essa abordagem contribui para a sustentabilidade ao reduzir potencialmente a necessidade
+                    de viagens adicionais e otimizar o uso do espaço na embarcação. Em última análise, ao combinar
+                    conveniência, eficiência e sustentabilidade, a nossa embarcação redefine os padrões do transporte de
+                    cargas, representando uma solução inovadora e integrada para as demandas logísticas modernas.
+                </div>
+            </div>
+            <h2>Nossa embarcação</h2>
+            <div class="content_boxes_two">
+                <div class="content_boxes_two_image">
+                    <img src="assets/img/embarcacao.png" alt="caminhoes_transito">
+                </div>
+                <div class="content_boxes_two_text">
+                    Nossa embarcação única destaca-se pela eficiência e compromisso ambiental. Com capacidade para até 15
+                    cargas, transportando 2 toneladas por carga, maximizamos a eficiência logística, reduzindo a pegada
+                    de carbono e promovendo um transporte de mercadorias mais sustentável. Essa abordagem não apenas
+                    atende às demandas modernas, mas também representa um passo significativo em direção a práticas.
+                </div>
+            </div>
+            <div class="extra_info">
+                <div class="extra_info_text">
+                    Além dos benefícios ambientais, o transporte fluvial destaca-se por sua capacidade de reduzir
+                    impactos adversos ao meio ambiente. Ao optar por essa modalidade, minimizamos a pegada de carbono e
+                    preservamos ecossistemas sensíveis. A escolha pelo transporte fluvial, portanto, não apenas atende
+                    às necessidades logísticas, mas também demonstra um compromisso efetivo com a sustentabilidade
+                    ambiental.
+                </div>
+                <div class="extra_info_image">
+                    <img src="assets/img/guindaste.png" alt="embarcações">
+                </div>
+            </div>
+            <h2>Rota</h2>
+            <div class="desc_rota">
+                <div class="rota_text">
+                    Nossa embarcação segue diariamente uma rota pré-definida, passando pelas seguintes cidades:
+                </div>
+                <div class="rota_image">
+                    <img src="assets/img/rota.png" alt="rota">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <?php include('includes/footer.php'); ?>
+    </footer>
+</body>
+
+</html>
